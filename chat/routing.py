4b859d353b2b6bfdcfd26ff051057915eb2d1954
hupender <hupender@ubuntu1.myguest.virtualbox.org>
2024-08-31 12:35:48 +0530

    group id will be uuid

--- chat/routing.py
+++ chat/routing.py
@@ -3,5 +3,5 @@ from chat.consumer import ChatConsumer, AppConsumer
 
 ws_patterns = [
 	# path("ws/chat/<str:room_id>/",ChatConsumer.as_asgi(), name="chat_app"),
-    path("ws/chat/<str:room_id>/",AppConsumer.as_asgi(), name="home")
+    path("ws/chat/<uuid:room_id>/",AppConsumer.as_asgi(), name="home")
 ]
