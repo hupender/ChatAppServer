52468826a9d0fac1baf954bc184aa163cf7ad010
hupender <hupender@ubuntu1.myguest.virtualbox.org>
2024-09-12 16:09:17 +0530

    order message by time

--- chat/api/chat.py
+++ chat/api/chat.py
@@ -86,9 +86,7 @@ class GetGroupMessage(BulkBaseView):
     def get(self, request, group_id, *args, **kwargs):
         user = self.schema.user
 
-        group_message_data = self.model.objects.filter(room=group_id)
-        print(group_message_data)
-        print(self.schema.dump(group_message_data))
+        group_message_data = self.model.objects.filter(room=group_id).order_by("update_ts")
 
         return JsonResponse(
             {"response": make_response(request, "GET", response_text=self.message, response_data=self.schema.dump(group_message_data)), "meta": {}}, status=200
